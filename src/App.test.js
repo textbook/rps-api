69f799b2cb00d9cb73cb6b5c46dc24a0c1966880
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-04-10 16:52:29 +0100

    Failing integration test for playing opponent

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import App from "./App";
 
@@ -11,5 +12,22 @@ describe("App", () => {
     expect(screen.getByTestId("opponent-name")).toHaveTextContent("Randy Wheeler");
     expect(screen.getByTestId("opponent-avatar"))
       .toHaveAttribute("src", "https://randomuser.me/api/portraits/thumb/men/12.jpg");
+  });
+
+  it("lets you play the random opponent", async () => {
+    const playerWeapon = "scissors";
+    const OUTCOMES = {
+      "rock": "Right wins!",
+      "paper": "Left wins!",
+      "scissors": "Draw",
+    };
+    render(<App />);
+
+    userEvent.selectOptions(screen.getByLabelText("Left"), playerWeapon);
+    userEvent.click(screen.getByText("Throw"));
+    await screen.findByTestId("opponent-weapon");
+
+    const opponentWeapon = screen.getByTestId("opponent-weapon").textContent
+    expect(screen.getByTestId("outcome")).toHaveTextContent(OUTCOMES[opponentWeapon]);
   });
 });
