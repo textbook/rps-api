113cb6baa634701e6f7ab675d530709cc3e0e21b
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-04-10 15:38:38 +0100

    Failing integration test for opponent

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("App", () => {
+  it("displays the opponent", async () => {
+    render(<App />);
+
+    await screen.findByTestId("opponent");
+
+    expect(screen.getByTestId("opponent-name")).toHaveTextContent("Randy Wheeler");
+    expect(screen.getByTestId("opponent-avatar"))
+      .toHaveAttribute("src", "https://randomuser.me/api/portraits/thumb/men/12.jpg");
+  });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+import fixture from "../cypress/fixtures/example.json";
+
+const server = setupServer(
+  rest.get("https://api.randomuser.me", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(fixture));
+  }),
+);
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
